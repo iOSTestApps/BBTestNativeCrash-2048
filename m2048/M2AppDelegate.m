cb3c6867606ffe87b317c0c63ed5710e3dd889fd
Rashin Arab <rashin@buddybuild.com>
2015-10-01 19:56:55 -0700

    forcing rebuild

--- m2048/M2AppDelegate.m
+++ m2048/M2AppDelegate.m
@@ -14,7 +14,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   [BuddyBuildSDK setup];
-  
+  // uh oh  
   int * test;
   *test = 5;
   
